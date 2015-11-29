ea1f711ac7d5f4308018ae4d2e4ac06fa3ad67c7
pocketberserker <pocketberserker@gmail.com>
2015-11-30 05:37:25 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Persimmon.Dried.Quotations
+
---
+++ src/Persimmon.Dried.Quotations/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+﻿namespace System
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitleAttribute("Persimmon.Dried.Quotations")>]
+[<assembly: AssemblyProductAttribute("Persimmon.Dried.Quotations")>]
+[<assembly: GuidAttribute("b27c7cb1-8c8d-41f9-85da-94ff8d0aab56")>]
+[<assembly: AssemblyDescriptionAttribute("Persimmon.Dried extension using Quotations")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
