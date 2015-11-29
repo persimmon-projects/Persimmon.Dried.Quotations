c6dd45669084d3b570186ac5ef1ea05d541ef6da
pocketberserker <pocketberserker@gmail.com>
2015-11-30 06:19:24 +0900

    implement

---
+++ src/Persimmon.Dried.Quotations/Quotations.fs
@@ -0,0 +1,65 @@
+﻿module Persimmon.Dried.Quotations
+
+open System.Reflection
+open Persimmon
+open Persimmon.Dried
+open Microsoft.FSharp.Quotations
+open Patterns
+
+let private (|PropertyName|) (info: PropertyInfo) = info.Name
+
+type QuotationPropertiesBuilder private (builder: PropertiesBuilder) =
+  new() = QuotationPropertiesBuilder(PropertiesBuilder())
+  new(name: string) = QuotationPropertiesBuilder(PropertiesBuilder(name))
+  member __.Yield(()) = builder.Yield(())
+  [<CustomOperation("verbosity")>]
+  member __.Verbosity(s, v) = builder.Verbosity(s, v)
+  [<CustomOperation("minSuccessfulTests")>]
+  member __.MinSuccessfulTests(s, v) = builder.MinSuccessfulTests(s, v)
+  [<CustomOperation("minSize")>]
+  member __.MinSize(s, v) = builder.MinSize(s, v)
+  [<CustomOperation("maxSize")>]
+  member __.MaxSize(s, v) = builder.MaxSize(s, v)
+  [<CustomOperation("prngState")>]
+  member __.PrngState(s, v) = builder.PrngState(s, v)
+  [<CustomOperation("workers")>]
+  member __.Workers(s, v) = builder.Workers(s, v)
+  [<CustomOperation("callback")>]
+  member __.Callback(s, v) = builder.Callback(s, v)
+  [<CustomOperation("maxDiscardRatio")>]
+  member __.MaxDiscardRatio(s, v) = builder.MaxDiscardRatio(s, v)
+  [<CustomOperation("apply")>]
+  member __.Apply<'T, 'U when 'U :> Prop>(s: PropertiesState<'T>, [<ReflectedDefinition(true)>] expr: Expr<'U>) =
+    let p =
+      match expr with
+      | ValueWithName(p, _, name) 
+      | WithValue(p, _, PropertyGet(_, PropertyName name, _)) ->
+        p :?> Prop |@ name
+      | WithValue(p, _, _)
+      | Value(p, _) -> p :?> Prop
+      | _ -> failwithf "expected value, but was %A" expr
+    { s with Properties = seq { yield! s.Properties; yield p } }
+  [<CustomOperation("applyReturn")>]
+  member __.ApplyReturn(s, [<ReflectedDefinition(true)>] expr: Expr<Prop<'T>>) =
+    let p =
+      match expr with
+      | ValueWithName(p, _, name)
+      | WithValue(p, _, PropertyGet(_, PropertyName name, _)) ->
+        let p = p :?> Prop<'T>
+        new Prop<'T>(p.Sample, p |@ name)
+      | WithValue(p, _, _)
+      | Value(p, _) -> p :?> Prop<'T>
+      | _ -> failwithf "expected value, but was %A" expr
+    {
+      RunnerParams = s.RunnerParams
+      PrettyParams = s.PrettyParams
+      Properties = seq { yield! s.Properties; yield p :> Prop }
+      Sample = p.Sample
+    }
+  member __.Delay(f: unit -> _) = f
+  member __.Run(f) = builder.Run(f)
+
+let property (name: string) = QuotationPropertiesBuilder(name)
+
+module UseTestNameByReflection =
+  let property = QuotationPropertiesBuilder()
---
+++ tests/Persimmon.Dried.Quotations.Tests/QuotationsTest.fs
@@ -0,0 +1,43 @@
+﻿namespace Persimmon.Dried.Quotations.Tests
+
+open System
+open Persimmon
+open Persimmon.Dried
+
+module QuotationsTest =
+
+  open Quotations
+  open UseTestNameByReflection
+
+  let ``number is zero`` = Prop.forAll Arb.int ((=) 0)
+
+  type QuotationPropertiesBuilder with
+    [<CustomOperation("test")>]
+    member __.Test(s: PropertiesState<_>, f) =
+      let meta = { Name = None; Parameters = [] }
+      TestCase(meta, fun () ->
+        let p = Prop.all s.Properties
+        let r = Runner.check s.RunnerParams p
+        let pr = p.Apply(Gen.Parameters.Default)
+        match assertPred <| f r pr with
+        | Passed () -> Done(meta, NonEmptyList.singleton (Passed ()), TimeSpan.Zero)
+        | NotPassed cause -> Done(meta, NonEmptyList.singleton (NotPassed cause), TimeSpan.Zero)
+      )
+    member __.Run(f: unit -> TestCase<_>) =
+      try f ()
+      with e -> TestCase.makeError None [] e
+
+  let ``record variable name`` = property {
+    apply ``number is zero``
+    test (fun r pr -> not <| Runner.Result.isPassed r && pr.Labels |> Seq.exists ((=) "number is zero"))
+  }
+
+  let ``record variable name and return value`` = property {
+    applyReturn ``number is zero``
+    test (fun r pr -> not <| Runner.Result.isPassed r && pr.Labels |> Seq.exists ((=) "number is zero"))
+  }
+
+  let ``value does not have label`` = property {
+    apply (Prop.forAll Arb.int ((=) 0))
+    test (fun r pr -> not <| Runner.Result.isPassed r && Set.isEmpty pr.Labels)
+  }
