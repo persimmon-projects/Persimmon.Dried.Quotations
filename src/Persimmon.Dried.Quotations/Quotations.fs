1a318c31d1adf231f10f7c2e95164541fd32bbd8
pocketberserker <pocketberserker@gmail.com>
2015-11-30 10:01:23 +0900

    refactor

--- src/Persimmon.Dried.Quotations/Quotations.fs
+++ src/Persimmon.Dried.Quotations/Quotations.fs
@@ -9,6 +9,22 @@ open Patterns
 let private (|PropertyName|) (info: PropertyInfo) = info.Name
 let private (|Property|) (info: PropertyInfo) = (info.GetValue(null, null), info.Name)
 
+let private appendLabel add cast = function
+| ValueWithName(p, _, name) 
+// lazy value
+| WithValue(p, _, PropertyGet(Some(PropertyGet (None, PropertyName name, [])), _, []))
+// local variable
+| WithValue(p, _, ValueWithName(_, _, name))
+// ref
+| WithValue(p, _, Call(_, _, [PropertyGet(_, PropertyName name, _)]))
+// get property
+| WithValue(p, _, PropertyGet(_, PropertyName name, _))
+| PropertyGet(_, Property(p, name), _) ->
+  add p name
+| WithValue(p, _, _)
+| Value(p, _) -> cast p
+| expr -> failwithf "expected value, but was %A" expr
+
 type QuotationPropertiesBuilder private (builder: PropertiesBuilder) =
   new() = QuotationPropertiesBuilder(PropertiesBuilder())
   new(name: string) = QuotationPropertiesBuilder(PropertiesBuilder(name))
@@ -31,34 +47,13 @@ type QuotationPropertiesBuilder private (builder: PropertiesBuilder) =
   member __.MaxDiscardRatio(s, v) = builder.MaxDiscardRatio(s, v)
   [<CustomOperation("apply")>]
   member __.Apply<'T, 'U when 'U :> Prop>(s: PropertiesState<'T>, [<ReflectedDefinition(true)>] expr: Expr<'U>) =
-    let p =
-      match expr with
-      | ValueWithName(p, _, name) 
-      | WithValue(p, _, PropertyGet(Some(PropertyGet (None, PropertyName name, [])), _, []))
-      | WithValue(p, _, ValueWithName(_, _, name))
-      | WithValue(p, _, Call(_, _, [PropertyGet(_, PropertyName name, _)]))
-      | WithValue(p, _, PropertyGet(_, PropertyName name, _))
-      | PropertyGet(_, Property(p, name), _) ->
-        p :?> Prop |@ name
-      | WithValue(p, _, _)
-      | Value(p, _) -> p :?> Prop
-      | _ -> failwithf "expected value, but was %A" expr
+    let p = appendLabel (fun p n -> p :?> Prop |@ n) (fun p -> p :?> Prop) expr
     { s with Properties = seq { yield! s.Properties; yield p } }
   [<CustomOperation("applyReturn")>]
   member __.ApplyReturn(s, [<ReflectedDefinition(true)>] expr: Expr<Prop<'T>>) =
     let p =
-      match expr with
-      | ValueWithName(p, _, name)
-      | WithValue(p, _, PropertyGet(Some(PropertyGet (None, PropertyName name, [])), _, []))
-      | WithValue(p, _, ValueWithName(_, _, name))
-      | WithValue(p, _, Call(_, _, [PropertyGet(_, PropertyName name, _)]))
-      | WithValue(p, _, PropertyGet(_, PropertyName name, _))
-      | PropertyGet(_, Property(p, name), _) ->
-        let p = p :?> Prop<'T>
-        new Prop<'T>(p.Sample, p |@ name)
-      | WithValue(p, _, _)
-      | Value(p, _) -> p :?> Prop<'T>
-      | _ -> failwithf "expected value, but was %A" expr
+      expr
+      |> appendLabel (fun p n -> let p = p :?> Prop<'T> in new Prop<'T>(p.Sample, p |@ n)) (fun p -> p :?> Prop<'T>)
     {
       RunnerParams = s.RunnerParams
       PrettyParams = s.PrettyParams
