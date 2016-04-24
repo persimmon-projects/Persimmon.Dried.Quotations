f84ba7764e24e2b4456564d692186c7d5ee5f2fe
pocketberserker <pocketberserker@gmail.com>
2016-04-24 14:35:52 +0900

    Bump version to 1.0.0

--- src/Persimmon.Dried.Quotations/AssemblyInfo.fs
+++ src/Persimmon.Dried.Quotations/AssemblyInfo.fs
@@ -6,11 +6,11 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyProductAttribute("Persimmon.Dried.Quotations")>]
 [<assembly: GuidAttribute("b27c7cb1-8c8d-41f9-85da-94ff8d0aab56")>]
 [<assembly: AssemblyDescriptionAttribute("Persimmon.Dried extension using Quotations")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
-    let [<Literal>] InformationalVersion = "0.1.0"
+    let [<Literal>] Version = "1.0.0"
+    let [<Literal>] InformationalVersion = "1.0.0"
